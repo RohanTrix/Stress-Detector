1594f634e1c20860cd6ad95c63254ce571f6b816
RohanTrix <52482556+RohanTrix@users.noreply.github.com>
2021-03-15 04:51:58 +0000

    Runner code working

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ emotion_classifier=load_model(emotion_model,compile=True)
 emotion_names=["angry","disgust","scared", "happy", "sad", "surprised",
  "neutral"]
 
-frame=cv2.imread('img1.jpeg')
+frame=cv2.imread('images/img1.jpeg')
 gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 faces=cascade.detectMultiScale(gray_frame,1.5,5)
 text=[]
@@ -29,7 +29,7 @@ for (x,y,w,h) in faces:
     for j in range(7):
         text.append(emotion_names[j]+" : "+str(percen[j]))
     for i in range(7):    
-        cv2.putText(frame,text[i],(5,i*15+15),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),2)
+        cv2.putText(frame,text[i],(5,i*15+15),cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,255,255),2)
     cv2.putText(frame,label,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,0),2)
     cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
 cv2.imwrite('results/res1.jpg', frame)
