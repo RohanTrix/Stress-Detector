9ead951933eacf34a64fc616156d208adbccdb7a
RohanTrix <52482556+RohanTrix@users.noreply.github.com>
2021-05-07 14:22:19 +0000

    Changes

--- eyebrow.py
+++ eyebrow.py
@@ -1,13 +1,27 @@
 from scipy.spatial import distance as dist
 from imutils import face_utils
 import numpy as np
+import math
 import imutils
 import time
 import dlib
 import cv2
+from cv2 import VideoWriter_fourcc, VideoWriter
 import matplotlib.pyplot as plt
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
+from flask import Flask, render_template, request, jsonify, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('templates/home.html')
+
+@app.route('/checkstress', methods = ['GET', 'POST'])
+def check_stress():
+    return render_template('templates/check.html')
+
+
 
 def eye_brow_distance(leye,reye):
     global points
@@ -36,8 +50,7 @@ def emotion_finder(faces,frame):
 def normalize_values(points,disp):
     normalized_value = abs(disp - np.min(points))/abs(np.max(points) - np.min(points))
     stress_value = np.exp(-(normalized_value))
-    print(stress_value)
-    if stress_value>=75:
+    if stress_value>=65:
         return stress_value,"High Stress"
     else:
         return stress_value,"low_stress"
@@ -45,10 +58,15 @@ def normalize_values(points,disp):
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 emotion_classifier = load_model("models/_mini_XCEPTION.102-0.66.hdf5", compile=False)
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('vid2.mp4')
 points = []
+stress_list = []
+stressval_list = []
+stressgraph = []
+size=0
 while(True):
     _,frame = cap.read()
+    if(not _): break
     frame = cv2.flip(frame,1)
     frame = imutils.resize(frame, width=500,height=500)
     
@@ -77,15 +95,22 @@ while(True):
 
         distq = eye_brow_distance(leyebrow[-1],reyebrow[0])
         stress_value,stress_label = normalize_values(points,distq)
+        print(stress_value)
+        #if stress_value!=1.0: stress_list.append(stress_list)
+        if math.isnan(stress_value):
+            continue
         cv2.putText(frame,"stress level:{}".format(str(int(stress_value*100))),(20,40),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-    cv2.imshow("Frame", frame)
+    height, width, layers = frame.shape
+    size = (width,height)
+    
+    stress_list.append(frame)
+    stressval_list.append(stress_value)
+    cv2.imwrite("Frame.jpg", frame)
+out = cv2.VideoWriter('resvid.avi',cv2.VideoWriter_fourcc(*'DIVX'), 10, size)
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
 cv2.destroyAllWindows()
 cap.release()
-plt.plot(range(len(points)),points,'ro')
-plt.title("Stress Levels")
-plt.imwrite()
+print("END REACHED")
+for i in range(len(stress_list)):
+    out.write(stress_list[i])
